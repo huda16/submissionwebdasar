904892dd093ecafe8e06d40a3647dd3dbcbb15c9
huda16 <62654718+huda16@users.noreply.github.com>
2020-07-21 17:58:53 +0700

    Add files via upload
    first

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+const a = document.getElementsByTagName('a');
+
+a[0].addEventListener('click', function() {
+	alert('Sejarah');
+});
+
+a[1].addEventListener('click', function() {
+	alert('Sistem Ubiquitous Computing');
+});
+
+a[2].addEventListener('click', function() {
+	alert('Teknologi Pendukung');
+});
